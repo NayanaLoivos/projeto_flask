c3874b3406e1d6a04e2aeb3f0e3aa2e838e3891d
Nayana Loivos <loivosdev@gmail.com>
2024-01-21 18:23:37 -0300

    added dynamic url - url_for()

--- davita.py
+++ davita.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, session, flash
+from flask import Flask, render_template, request, redirect, session, flash, url_for
 class Unidade:
     def __init__(self, name, street):
         self.name = name
@@ -10,13 +10,15 @@ lista = [unidade1]
 app = Flask(__name__) #__name__ faz referência ao próprio arquivo
 app.secret_key = 'goofy'
 @app.route('/') #quando criamos uma nova rota, precisamos de uma função que crie uma nova rota.
-def ola():
+def index():
     return render_template('clinicas.html', titulo="Unidades", unidades=lista)
 
 @app.route('/new-unidade')
 def newClinica():
     if 'usuario_logado' not in session or session['usuario_logado'] == None:
-        return redirect('/login?proxima=new-unidade')
+        # return redirect('/login?proxima=new-unidade')
+        return redirect(url_for('login', proxima=url_for('newClinica')))
+
     return render_template('new-clinica.html', titulo='Nova unidade Davita')
 
 @app.route('/create', methods=['POST',])
@@ -25,7 +27,8 @@ def create():
     street = request.form['street']
     unidade = Unidade(name, street)
     lista.append(unidade)
-    return redirect('/')
+    # return redirect('/')
+    return redirect(url_for('index'))
 
 @app.route('/login')
 def login():
@@ -38,16 +41,18 @@ def autenticar():
         session['usuario_logado'] = request.form['usuario']
         flash(session['usuario_logado'] + 'logado com sucesso!')
         proxima_pagina = request.form['proxima']
-        return redirect('/{}'.format(proxima_pagina))
+        # return redirect('/{}'.format(proxima_pagina))
+        return redirect(proxima_pagina)
     else:
-        session['usuario_logado'] = None
         flash('Login ou senha inválidos!!!')
-        return redirect('/login')
+        # return redirect('/login')
+        return redirect(url_for('login'))
 @app.route('/logout')
 def logout():
     session['usuario_logado'] = None
     flash('Usuário deslogado!')
-    return redirect('/login')
+    # return redirect('/login')
+    return redirect(url_for('index'))
 
 app.run(debug=True) #aqui eu poderia definir (host='0.0.0.0', port=8080), mas não levar essas configurações para produção.
 
